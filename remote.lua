f9761ee6f97a57c61e1769573c8bd72df2d478d9
Ondrej Cifka <ondra@cifka.com>
2016-08-11 17:46:06 +0200

    Initial commit

---
+++ remote.lua
@@ -0,0 +1,60 @@
+
+local log = require("log");
+local keyboard = libs.keyboard;
+local script = libs.script;
+
+local last_volume = 0;
+
+function get_volume()
+	volume = script.shell(
+		'amixer -c 0 get Master | grep -Eo "[0-9]+%" | tr -d "%" | head -1');
+	return tonumber(volume);
+end
+
+function set_volume(value)
+	os.script(string.format("amixer -c 0 sset Master %.2f%%", value));
+end
+
+--@help Lower system volume
+actions.volume_down = function()
+	last_volume = get_volume();
+	set_volume(math.max(0, last_volume - 10));
+end
+
+--@help Mute system volume
+actions.volume_mute = function()
+	volume = get_volume();
+	if volume <= 0 then
+		set_volume(last_volume);
+		last_volume = 0;
+	else
+		last_volume = volume;
+		set_volume(0);
+	end
+end
+
+--@help Raise system volume
+actions.volume_up = function()
+	last_volume = get_volume();
+	set_volume(math.min(100, last_volume + 10));
+end
+
+--@help Previous track
+actions.previous = function()
+	keyboard.press("mediaprevious");
+end
+
+--@help Next track
+actions.next = function()
+	keyboard.press("medianext");
+end
+
+--@help Stop playback
+actions.stop = function()
+	keyboard.press("mediastop");
+end
+
+--@help Toggle playback state
+actions.play_pause = function()
+	keyboard.press("mediaplaypause");
+end
